163e6225dcf49fc252a8a7de1ebe06d9cd7691eb
george-mountain <georgejiokem@gmail.com>
2024-07-24 18:12:43 +0900

    api login request update

--- react-frontend/services/api.js
+++ react-frontend/services/api.js
@@ -1,37 +1,32 @@
-
 import api from "./http.service";
 
 export const request = {
   getInfo: () => api.get("/users/me/"),
   signup: (data) => api.post("/users/", data),
-  login: (data) => {
-    const formData = new URLSearchParams();
-    formData.append("username", data.username);
-    formData.append("password", data.password);
-    return api.post("/token", formData, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    });
-  },
+  login: (data) => api.post("/token", data),
   getUserItems: () => api.get("/users/me/items/"),
   createUserItem: (data) => api.post("/users/me/items/", data),
   deleteUserItem: (itemId) => api.delete(`/users/me/items/${itemId}`),
   requestPasswordReset: (data) => api.post("/users/reset-password/", data),
-  requestPasswordReset: (data) => api.post("/users/reset-password/", {
-    email: data.email,
-    frontend_url: data.frontendUrl 
-  }),
-  
-  resetPassword: (data) => api.post(`/users/reset-password/${data.token}`, { new_password: data.new_password }),
+  requestPasswordReset: (data) =>
+    api.post("/users/reset-password/", {
+      email: data.email,
+      frontend_url: data.frontendUrl,
+    }),
+
+  resetPassword: (data) =>
+    api.post(`/users/reset-password/${data.token}`, {
+      new_password: data.new_password,
+    }),
 
   // Chat related requests
   chat: (data) => api.post("/chat", data),
-  getChats: ({ user_id,page, pageSize }) => api.get(`/chats?user_id=${user_id}&page=${page}&page_size=${pageSize}`),
-  getChatMessages: ({ chatId, page, pageSize }) => api.get(`/chats/${chatId}/messages?page=${page}&page_size=${pageSize}`),
-
+  getChats: ({ user_id, page, pageSize }) =>
+    api.get(`/chats?user_id=${user_id}&page=${page}&page_size=${pageSize}`),
+  getChatMessages: ({ chatId, page, pageSize }) =>
+    api.get(`/chats/${chatId}/messages?page=${page}&page_size=${pageSize}`),
 
   deleteChat: (chatId) => api.delete(`/chats/${chatId}`),
-  archiveAllChats: (userId) => api.post(`/chats/archive_all`, { user_id: userId }),
-
+  archiveAllChats: (userId) =>
+    api.post(`/chats/archive_all`, { user_id: userId }),
 };
